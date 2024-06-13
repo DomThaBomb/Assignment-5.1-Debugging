ce678688ddd35ac4abd453d80190efca9329536a
DomThaBomb <djamadotramondo1005@gmail.com>
2024-06-13 15:00:03 -0700

    Create JavaScript game.js

---
+++ JavaScript game.js
@@ -0,0 +1,14 @@
+//logic part of the code
+let Money = 1000;
+function grabmoney(){
+    Money -= 1000;
+    console.log("You gained more money")
+}
+
+function gainnmoney(){
+    console.log(Money);
+}
+
+gainedmoney();
+grabmoney();
+gainedmoney();
